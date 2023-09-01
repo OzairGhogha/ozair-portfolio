d93f4c893070998ffbc00355bf3d6fc161db069d
Ozair Ghogha <O.M.Y.Ghogha@edu.salford.ac.uk>
2023-09-01 14:56:48 +0100

    fixed icon rendering

--- pages/_app.js
+++ pages/_app.js
@@ -20,8 +20,11 @@ function MyApp({ Component, pageProps }) {
         <motion.div key={router.route} className='h-full'>
           <Transition />
           <Head>
-        <link rel="shortcut icon" href="/favicon.ico" />
-      </Head>
+          <link rel="shortcut icon" href="/images/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
+</Head>
           <Component {...pageProps} />
         </motion.div>
       </AnimatePresence>
