d68e1df6456357eb10d0bbd457a1f5afa708d34e
Ozair Ghogha <O.M.Y.Ghogha@edu.salford.ac.uk>
2023-09-01 15:29:07 +0100

    updated meta tags

--- pages/_app.js
+++ pages/_app.js
@@ -20,11 +20,14 @@ function MyApp({ Component, pageProps }) {
         <motion.div key={router.route} className='h-full'>
           <Transition />
           <Head>
+          <title>Ozair Ghogha - Portfolio</title>
+          <meta name="author" content="Ozair Ghogha"/>
+          <meta name="description" content="Ozair Ghogha is a cloud engineer who helps clients with their technology needs. He has a strong background in computer science and a passion for learning new skills. He has worked on various projects that you can see on his website."/>
           <link rel="shortcut icon" href="/images/favicon.ico" />
           <link rel="apple-touch-icon" sizes="180x180" href="/images/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon-32x32.png"/>
           <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon-16x16.png"/>
-</Head>
+          </Head>
           <Component {...pageProps} />
         </motion.div>
       </AnimatePresence>
